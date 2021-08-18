9fb7e257e2cae72d9f3e6e9e90a6a3e3cbd8197b
liujh <liujh@gzsendi>
2021-08-18 13:45:01 +0800

    init

---
+++ src/main/java/cn/gzsendi/activitidemotest/ActivitidemotestApplication.java
@@ -0,0 +1,13 @@
+package cn.gzsendi.activitidemotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitidemotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitidemotestApplication.class, args);
+	}
+
+}
